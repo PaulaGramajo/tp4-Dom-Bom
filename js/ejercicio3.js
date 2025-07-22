346030417f389b870c7d12de3ab4818482131bea
Paula Gramajo <paulagramajo.tuc@gmail.com>
2025-07-22 12:49:09 -0300

    hice cambios para eliminar la tarea, falta completar

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,5 +1,18 @@
 const crearTarea = document.getElementById('formTareas')
+const tareas = document.querySelector('.row-gap-3');
 
+tareas.addEventListener('click', function(Event){
+    const clickedCard = Event.target.closest('card');
+    if(clickedCard){
+        const allCards = document.querySelectorAll('.card');
+        allCards.forEach(card => {
+            if (card !== clickedCard) {
+                card.classList.remove('selected');
+            }
+            })
+        clickedCard.classList.toggle('selected')
+    }
+})
 
 formTareas.addEventListener('submit', function(Event){
     Event.preventDefault();
@@ -7,33 +20,35 @@ formTareas.addEventListener('submit', function(Event){
     const inputTarea = document.getElementById('tarea').value.trim();
     const divcol = document.createElement('div');
     const divcard = document.createElement("div");
-    const divcardBody = document.createElement("div"); 
-    const h5 = document.createElement("h5"); 
+    const divcardBody = document.createElement("div");  
     const p = document.createElement("p");
     const btnEliminar = document.createElement('button')
+    const inputCheckbox = document.createElement('input')
     
-    divcol.className = "col-md-4 col-lg-3"; 
+    divcol.className = "col-12"; 
     divcard.className = "card h-100";
-    divcardBody.className = "card-body";
-    h5.className = "card-title";
-    h5.textContent = "Tarea";
-    p.className = "card-text";
+    divcardBody.className = "card-body d-flex justify-content-between align-items-center";
+    p.className = "card-text ";
+    inputCheckbox.type = 'checkbox'
     p.textContent = inputTarea;
 
     btnEliminar.className = 'btn btn-danger btn-sm mt-3'
     btnEliminar.textContent = 'Eliminar'
 
     btnEliminar.addEventListener('click', function(){
-        divcol.remove();
-    })
+        const cardToRemove = Event.target.closest('.col-12');
+        if (cardToRemove) {
+            cardToRemove.remove();
+        }
+    });
 
     const row = document.querySelector(".row-gap-3");
   
   row.appendChild(divcol);
   divcol.appendChild(divcard);
   divcard.appendChild(divcardBody);
-  divcardBody.appendChild(h5);
+  divcardBody.appendChild(inputCheckbox);
   divcardBody.appendChild(p);
   divcardBody.appendChild(btnEliminar);
-  formularioTarea.reset();
+  formTareas.reset();
 })
